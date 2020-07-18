8f4d1c7731fb6858f4e79ac8c83780fab64ddc6a
Junsung Park <zeipar@gmail.com>
2020-07-18 11:05:13 -0700

    Api fetches a random item.

--- src/components/ApiFetcher.js
+++ src/components/ApiFetcher.js
@@ -2,23 +2,26 @@ import React, { useState, useEffect } from "react";
 
 export default function ApiFetcher() {
   const [itemList, setItemList] = useState([]);
+  // const [randArrIndex, setRandArrIndex] = useState([]);
 
   useEffect(() => {
     console.log("useEffect fired!");
-    const targetAPI = "https://pokeapi.co/api/v2/item";
+    const targetAPI = "https://pokeapi.co/api/v2/item?limit=1000";
 
     fetch(targetAPI)
       .then(data => data.json())
-      .then(jsonData => setItemList(jsonData));
+      .then(jsonData =>
+        setItemList(jsonData.results[Math.floor(Math.random() * 4)])
+      );
   }, []);
 
   console.log(itemList);
+
   return (
     <div>
       {/* {itemList.map(item => (
         <div key={item.name}>
           <p>{item.name}</p>
-          <p>{item.url}</p>
         </div>
       ))} */}
     </div>
